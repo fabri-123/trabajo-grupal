6acf9919a0b3e146a73dc1bad8b1eda85b8d47ee
fabri-123 <fabricioenavarro@gmail.com>
2025-04-29 19:03:58 -0300

    aporte mio

---
+++ fabri.js
@@ -0,0 +1,18 @@
+// trabajo realizado por: fabricio navarro
+
+// punto numero 3
+
+alert("la suma de 3 + 5 es : " + (3+5));
+
+
+// punto numero 4
+
+nombreDeUsuario = prompt("ingrese su nombre de usuario: ");
+while (nombreDeUsuario == null || nombreDeUsuario == "") {
+    alert("el nombre de usuario no puede estar vacio")
+    nombreDeUsuario = prompt("ingrese su nombre de usuario: ")
+} ;
+alert("su nombre de usuario es : " + nombreDeUsuario);
+   
+
+ 
